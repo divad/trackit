ef8b421c9f18da34753c9dd984f1335724732533
David Bell <dave@evad.info>
2014-12-08 18:07:39 +0000

    apache restart added; trackitd now supports new 'web_security' flag

--- pyro.py
+++ pyro.py
@@ -4,4 +4,8 @@ import Pyro4
 svr=Pyro4.Proxy('PYRO:trackit-daemon@localhost:1888')  
 svr._pyroHmacKey = "changeme"
 print svr.trac_auth_config('tux2')
+print svr.trac_auth_config('apache')
+
+
+print svr.server_graceful()
 
