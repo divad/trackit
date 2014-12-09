117862cb5d571595a821f4a1d30c15997ac441e2
David Bell <dave@evad.info>
2014-12-09 13:26:04 +0000

    More updates

--- pyro.py
+++ pyro.py
@@ -5,6 +5,7 @@ svr=Pyro4.Proxy('PYRO:trackit-daemon@localhost:1888')
 svr._pyroHmacKey = "changeme"
 print svr.trac_auth_config('tux2')
 print svr.trac_auth_config('apache')
+print svr.regenerate_authz_file()
 
 
 print svr.server_graceful()
