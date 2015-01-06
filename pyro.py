7b0a2cf1dfc3d7e998cc90cdd162445888c5d220
David Bell <dave@evad.info>
2015-01-06 23:30:00 +0000

    Very early/basic/rough git support.

--- pyro.py
+++ pyro.py
@@ -3,10 +3,10 @@ import Pyro4
 
 svr=Pyro4.Proxy('PYRO:trackit-daemon@localhost:1888')  
 svr._pyroHmacKey = "askgjio32904234kjsdflkjsfd09210jlkasd"
-print svr.trac_auth_config('tux2')
+print svr.repo_create('git1','git','none','db2z07')
 #print svr.trac_auth_config('apache')
 #print svr.regenerate_authz_file()
 
 
-print svr.server_graceful()
+svr.server_graceful()
 
